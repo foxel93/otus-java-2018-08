c6200e75a647cb97b5a4367a6e437f5a4eda159a
Василий <42024696+foxel93@users.noreply.github.com>
2018-09-02 04:52:14 +1100

    ДЗ 1 (Обфускация)

---
+++ work_1.1.2/src/main/java/ru/otus/Main.java
@@ -0,0 +1,14 @@
+package ru.otus;
+
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        MathNew.add(1,2);
+        MathNew.div(2,1);
+    }
+
+
+
+}
---
+++ work_1.1.2/src/main/java/ru/otus/MathNew.java
@@ -0,0 +1,6 @@
+package ru.otus;
+
+public class MathNew {
+    public static int add(int a, int b) {return a+b;}
+    public static int div(int a, int b) {return a/b;}
+}
